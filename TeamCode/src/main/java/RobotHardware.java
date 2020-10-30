70fae81808c27ae354d058f2b2c248f3aa4c234e
parkerduggins <parkerduggins@gmail.com>
2020-10-29 20:28:59 -0400

    Robot Hardware: defined and initialized a servo and a motor for the wobble arm. defined and initialized servo for the ring scooper. Teleop: Assigned action to buttons.

---
+++ TeamCode/src/main/java/RobotHardware.java
@@ -0,0 +1,209 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class RobotHardware
+{
+    public DcMotor  leftFrontDrive  = null;
+    public DcMotor  rightFrontDrive = null;
+    public DcMotor  leftRearDrive  = null;
+    public DcMotor  rightRearDrive = null;
+    public DcMotor  wobbleMotor = null;
+
+    public Servo wobbleServo = null;
+    public Servo scoopServo = null;
+
+    BNO055IMU imu;
+
+    DistanceSensor leftDistanceSensor;
+    DistanceSensor rightDistanceSensor;
+    DistanceSensor rearDistanceSensor;
+    DistanceSensor frontDistanceSensor;
+
+    HardwareMap hwMap           =  null;
+
+    //Hardware constants
+
+    public static final double TELEOPDEADZONE = 0.05;
+    public static final double NOTTURBOFACTOR = 0.5;
+    public static final int CLICKS_PER_INCH = 45;
+    public static final int STRAFE_CLICKS_PER_INCH = 48;
+
+
+    public final double HIGH_TURN_POWER = 0.3;
+    public final double MEDIUM_TURN_POWER = 0.12;
+    public final double LOW_TURN_POWER = 0.04;
+
+    public final double WOBBLE_SERVO_OPEN = 0.24;
+    public final double WOBBLE_SERVO_CLOSED = 1.00;
+    public final double SCOOPING_POSITION = 1.00;
+    public final double DROPPING_POSITION = 0.17;
+
+    public RobotHardware(){
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        wobbleServo = hwMap.get(Servo.class, "wobbleServo");
+        wobbleMotor = hwMap.get(DcMotor.class, "wobbleMotor");
+        scoopServo = hwMap.get(Servo.class, "scoopServo");
+        // Define and Initialize Motors
+//        leftFrontDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+//        rightFrontDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+//        leftRearDrive  = hwMap.get(DcMotor.class, "leftRearDrive");
+//        rightRearDrive = hwMap.get(DcMotor.class, "rightRearDrive");
+
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
+//        frontDistanceSensor = hwMap.get(DistanceSensor.class, "frontDistanceSensor");
+//        leftDistanceSensor = hwMap.get(DistanceSensor.class, "leftDistanceSensor");
+//        rightDistanceSensor = hwMap.get(DistanceSensor.class, "rightDistanceSensor");
+//        rearDistanceSensor = hwMap.get(DistanceSensor.class, "rearDistanceSensor");
+
+
+//        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rightRearDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+//        leftFrontDrive.setPower(0);
+//        rightFrontDrive.setPower(0);
+//        leftRearDrive.setPower(0);
+//        rightRearDrive.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+//        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void teleopInit(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        wobbleServo = hwMap.get(Servo.class, "wobbleServo");
+        wobbleMotor = hwMap.get(DcMotor.class, "wobbleMotor");
+        scoopServo = hwMap.get(Servo.class, "scoopServo");
+
+
+        // Define and Initialize Motors
+//        leftFrontDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
+//        rightFrontDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
+//        leftRearDrive  = hwMap.get(DcMotor.class, "leftRearDrive");
+//        rightRearDrive = hwMap.get(DcMotor.class, "rightRearDrive");
+//
+//        rightFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+//        rightRearDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+//        leftFrontDrive.setPower(0);
+//        rightFrontDrive.setPower(0);
+//        leftRearDrive.setPower(0);
+//        rightRearDrive.setPower(0);
+
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+//        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+//    public void resetEncoder () {
+//        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//
+//    }
+//
+//    public void setupDriveTrain () {
+//        resetEncoder();
+//
+//        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        leftRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        rightRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//    }
+//
+//
+//    public void runUsingEncoder () {
+//        resetEncoder();
+//
+//        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//    }
+//
+//    public void stop () {
+//        leftFrontDrive.setPower(0);
+//        rightFrontDrive.setPower(0);
+//        leftRearDrive.setPower(0);
+//        rightRearDrive.setPower(0);
+//    }
+
+//    public double getHeading() {
+//        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+//        return angles.firstAngle;
+//    }
+
+}
---
+++ TeamCode/src/main/java/TeleOp.java
@@ -0,0 +1,214 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Teleop", group="Teleop")
+
+public class TeleOp extends LinearOpMode {
+
+    RobotHardware robot = new RobotHardware();
+
+
+    @Override
+    public void runOpMode() {
+//        double ly;
+//        double rx;
+//        double lx;
+//        boolean slow_mode;
+//        boolean normal_mode;
+
+        robot.teleopInit(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            if (gamepad2.left_bumper) {
+                robot.wobbleServo.setPosition(robot.WOBBLE_SERVO_CLOSED);
+            }
+
+            if (gamepad2.left_trigger > 0.8) {
+                robot.wobbleServo.setPosition(robot.WOBBLE_SERVO_OPEN);
+            }
+
+            if (Math.abs(gamepad2.left_stick_y) > robot.TELEOPDEADZONE) {
+                robot.wobbleMotor.setPower(-gamepad2.left_stick_y);
+
+            }
+            else robot.wobbleMotor.setPower(0);
+
+
+            if(gamepad2.right_bumper) {
+                robot.scoopServo.setPosition(robot.SCOOPING_POSITION);
+            }
+
+            if (gamepad2.right_trigger > 0.8) {
+                robot.scoopServo.setPosition(robot.DROPPING_POSITION);
+            }
+
+//            slow_mode = false;
+//            normal_mode = false;
+//
+//            //strafe and turn right slowly with dpad
+//            if (gamepad1.dpad_right ||
+//                    gamepad1.dpad_left ||
+//                    gamepad1.dpad_up ||
+//                    gamepad1.dpad_down) {
+//                slow_mode = true;
+//                normal_mode = false;
+//
+//
+//                if (gamepad1.dpad_right) {
+//                    //turn right slowly with dpad
+//                    if (gamepad1.b) {
+//                        robot.leftFrontDrive.setPower(0.05);
+//                        robot.rightFrontDrive.setPower(-0.05);
+//                        robot.leftRearDrive.setPower(0.05);
+//                        robot.rightRearDrive.setPower(-0.05);
+//                    }
+//                    //strafe right slowly with dpad
+//                    else {
+//                        robot.leftFrontDrive.setPower(0.05);
+//                        robot.rightFrontDrive.setPower(-0.05);
+//                        robot.leftRearDrive.setPower(-0.05);
+//                        robot.rightRearDrive.setPower(0.05);
+//                    }
+//                    //strafe and turn left slowly with dpad
+//                } else if (gamepad1.dpad_left) {
+//                    //turn left slowly with dpad
+//                    if (gamepad1.b) {
+//                        robot.leftFrontDrive.setPower(-0.05);
+//                        robot.rightFrontDrive.setPower(0.05);
+//                        robot.leftRearDrive.setPower(-0.05);
+//                        robot.rightRearDrive.setPower(0.05);
+//                    }
+//                    //strafe left slowly with dpad
+//                    else {
+//                        robot.leftFrontDrive.setPower(-0.05);
+//                        robot.rightFrontDrive.setPower(0.05);
+//                        robot.leftRearDrive.setPower(0.05);
+//                        robot.rightRearDrive.setPower(-0.05);
+//                    }
+//                    //drive forward slowly with dpad
+//                } else if (gamepad1.dpad_up) {
+//                    robot.leftFrontDrive.setPower(0.07);
+//                    robot.rightFrontDrive.setPower(0.07);
+//                    robot.leftRearDrive.setPower(0.07);
+//                    robot.rightRearDrive.setPower(0.07);
+//                    //drive backward slowly with dpad
+//                } else if (gamepad1.dpad_down) {
+//                    robot.leftFrontDrive.setPower(-0.07);
+//                    robot.rightFrontDrive.setPower(-0.07);
+//                    robot.leftRearDrive.setPower(-0.07);
+//                    robot.rightRearDrive.setPower(-0.07);
+//                }
+//            }
+//            // Normal mode
+//            else {
+//                ly = -gamepad1.left_stick_y; //drive forward
+//                lx = -gamepad1.left_stick_x; //strafe
+//                rx = -gamepad1.right_stick_x; //turn
+//
+//                if (Math.abs(ly) > robot.TELEOPDEADZONE ||
+//                        Math.abs(lx) > robot.TELEOPDEADZONE ||
+//                        Math.abs(rx) > robot.TELEOPDEADZONE) {
+//
+//                    normal_mode = true;
+//                    slow_mode = false;
+//                    // Compute the drive speed of each drive motor based on formula from redit
+//                    double FL_power_raw = ly - lx - (rx * .7f);
+//                    double FR_power_raw = ly + lx + (rx * .7f);
+//                    double RL_power_raw = ly + lx - (rx * .7f);
+//                    double RR_power_raw = ly - lx + (rx * .7f);
+//
+//                    //Clip the values generated by the above formula so that they never go outisde of -1 to 1
+//                    double FL_power = Range.clip(FL_power_raw, -1, 1);
+//                    double FR_power = Range.clip(FR_power_raw, -1, 1);
+//                    double RL_power = Range.clip(RL_power_raw, -1, 1);
+//                    double RR_power = Range.clip(RR_power_raw, -1, 1);
+//
+//                    if (!gamepad1.a) { //This is normal mode
+//                        FL_power = FL_power * robot.NOTTURBOFACTOR;
+//                        FR_power = FR_power * robot.NOTTURBOFACTOR;
+//                        RL_power = RL_power * robot.NOTTURBOFACTOR;
+//                        RR_power = RR_power * robot.NOTTURBOFACTOR;
+//                    }
+//
+//                    robot.leftFrontDrive.setPower(FL_power);
+//                    robot.rightFrontDrive.setPower(FR_power);
+//                    robot.leftRearDrive.setPower(RL_power);
+//                    robot.rightRearDrive.setPower(RR_power);
+//
+//                }
+//                if (slow_mode == false && normal_mode == false) {
+//                    robot.leftFrontDrive.setPower(0);
+//                    robot.rightFrontDrive.setPower(0);
+//                    robot.leftRearDrive.setPower(0);
+//                    robot.rightRearDrive.setPower(0);
+//                }
+//            }
+//
+//
+//            if (Math.abs(gamepad2.right_stick_y) > robot.TELEOPDEADZONE) { //Move the lift up and down
+//                robot.liftMotor.setPower(Range.clip(gamepad2.right_stick_y, -1.0, 1.0));
+//            } else {
+//                robot.liftMotor.setPower(0);
+//            }
+//
+//
+//            if (gamepad1.left_trigger > 0.5) { // Grab the foundation
+//                robot.rightFoundationServo.setPosition(robot.RIGHT_FOUNDATION_SERVO_DOWN);
+//                robot.leftFoundationServo.setPosition(robot.LEFT_FOUNDATION_SERVO_DOWN);
+//            }
+//
+//            if (gamepad1.left_bumper) { // Let go of the foundation
+//                robot.rightFoundationServo.setPosition(robot.RIGHT_FOUNDATION_SERVO_UP);
+//                robot.leftFoundationServo.setPosition(robot.LEFT_FOUNDATION_SERVO_UP);
+//            }
+//
+//
+//            if (gamepad2.y) {
+//                robot.capStoneServo.setPosition(robot.CAPSTONE_SERVO_IN);
+//            }
+//
+//            if (gamepad2.a) {
+//                robot.capStoneServo.setPosition(robot.CAPSTONE_SERVO_OUT);
+//            }
+//
+//            if (gamepad2.b && !gamepad2.start) {
+//                robot.capStoneHolder.setPosition(robot.CAPSTONE_HOLDER_RELEASE);
+//            }
+//            if (gamepad2.x) {
+//                robot.capStoneHolder.setPosition(robot.CAPSTONE_HOLDER_GRAB);
+//            }
+//
+//
+//            if (gamepad2.left_bumper) { //Let go of the block
+//                robot.blockGrabbingServo.setPosition(robot.BLOCK_SERVO_RELEASE);
+//
+//            } else if (gamepad2.left_trigger > 0.5) { //Grab a block
+//                robot.blockGrabbingServo.setPosition(robot.BLOCK_SERVO_GRAB);
+//            }
+//
+//            if (gamepad2.dpad_up) {
+//                robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_SERVO_UP);
+//            }
+//
+//            if (gamepad2.dpad_down) {
+//                robot.blockFlippingServo.setPosition(robot.LIFT_BLOCK_SERVO_DOWN);
+//            }
+//
+//
+//            // Pace this loop so jaw action is reasonable speed.
+//            sleep(50);
+//
+       }
+    }
+}
