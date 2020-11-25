38e07caaf0f4bca591828b86047864b8ed61ac9b
DillonWgithub <dpwa1k3r@gmail.com>
2020-11-24 20:32:16 -0500

    Imported Strafe, added pseudocode for a driveTo program

--- TeamCode/src/main/java/AutonomousProgram.java
+++ TeamCode/src/main/java/AutonomousProgram.java
@@ -2,6 +2,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @Autonomous(name = "Autonomous")
 
 public class AutonomousProgram extends LinearOpMode {
@@ -10,6 +12,7 @@ public class AutonomousProgram extends LinearOpMode {
     Drive drive = new Drive(robot, telemetry, this);
     Shooter shooter = new Shooter(robot, telemetry, this);
     GyroTurn gyroTurn = new GyroTurn(robot, telemetry, this);
+    Strafe strafe = new Strafe(robot, telemetry, this);
 
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
@@ -30,9 +33,17 @@ public class AutonomousProgram extends LinearOpMode {
         shooter.triggerReset();
         shooter.wheelsOff();
 
+        //driveTo (either drive or strafe, or both)
         drive.backward(0.25, 10);
         wobble.moveArm(robot.WOBBLE_ARM_DOWN);
         wobble.release();
         wobble.moveArm(robot.WOBBLE_ARM_UP);
+
+
+        //driveTo (drive, turn, drive forward if needed)
+        //wobble.moveArm(robot.WOBBLE_ARM_DOWN);
+        //wobble.something
+        //wobble.moveArm(robot.WOBBLE_ARM_UP);
+        //gyroTurn.absolute(90);
     }
 }
