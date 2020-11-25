c2bc25628850f0f05fe3fcf125ce6353c82f8fff
parkerduggins <parkerduggins@gmail.com>
2020-11-24 20:29:08 -0500

    Changed the shooter angle for the high position. Changed the distance sensor output from CM to Inches.

--- TeamCode/src/main/java/TeleOp.java
+++ TeamCode/src/main/java/TeleOp.java
@@ -216,10 +216,10 @@ public class TeleOp extends LinearOpMode {
             telemetry.addData("liftServoPosition", robot.liftServo.getPosition());
 
 
-            telemetry.addData("front distance", robot.frontDistanceSensor.getDistance(DistanceUnit.CM));
-            telemetry.addData("right distance", robot.rightDistanceSensor.getDistance(DistanceUnit.CM));
-            telemetry.addData("rear distance", robot.rearDistanceSensor.getDistance(DistanceUnit.CM));
-            telemetry.addData("left distance", robot.leftDistanceSensor.getDistance(DistanceUnit.CM));
+            telemetry.addData("front distance", robot.frontDistanceSensor.getDistance(DistanceUnit.INCH));
+            telemetry.addData("right distance", robot.rightDistanceSensor.getDistance(DistanceUnit.INCH));
+            telemetry.addData("rear distance", robot.rearDistanceSensor.getDistance(DistanceUnit.INCH));
+            telemetry.addData("left distance", robot.leftDistanceSensor.getDistance(DistanceUnit.INCH));
             telemetry.update();
 
 //                }
