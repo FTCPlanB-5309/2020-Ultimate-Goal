a8d9fbc525a56a2e5d46e4209ee4a924007cc937
jessicF <jessica.d.frank@gmail.com>
2020-12-31 16:06:18 -0500

    no message

--- TeamCode/src/main/java/TwoWobbleAuto.java
+++ TeamCode/src/main/java/TwoWobbleAuto.java
@@ -23,43 +23,78 @@ public class TwoWobbleAuto extends LinearOpMode{
         waitForStart();
 
         WobbleTarget target = wobbleFinder.search();
+//        WobbleTarget target = WobbleTarget.C;
         telemetry.addData("position", target);
         telemetry.update();
 
         robot.scoopServo.setPosition(robot.DRIVING_POSITION);
         wobble.moveArm(robot.WOBBLE_ARM_UP);
 
-//        strafe.left(0.5, ((int)robot.getDistanceToWall(15) - 20));
-//        wobble.place1stWobble(target);
-//        wobble.moveArm(robot.WOBBLE_ARM_DOWN);
-//        wobble.release();
-//        wobble.moveArm(robot.WOBBLE_ARM_UP);
-
         //Retrieve second wobble, then place in A
         if (target == WobbleTarget.A) {
             drive.backward(0.8, 55);
-//            telemetry.addData("Wall Distance", robot.leftDistanceSensor.getDistance(DistanceUnit.INCH));
-//            telemetry.addData("Wall Distance Two", robot.leftDistanceSensorTwo.getDistance(DistanceUnit.INCH));
-//            telemetry.update();
             wobble.moveArm(robot.WOBBLE_ARM_DOWN);
+            Thread.sleep(1000);
             wobble.release();
             wobble.moveArm(robot.WOBBLE_ARM_UP);
+            Thread.sleep(500);
 
             //Second wobble
 //            strafe.right(0.5,6);
             robot.rightGrabberServo.setPosition(robot.RIGHT_GRABBER_OPEN);
             robot.leftGrabberServo.setPosition(robot.LEFT_GRABBER_OPEN);
-            drive.forward(0.8, 46);
             robot.scoopServo.setPosition(robot.SCOOPING_POSITION);
+            drive.forward(0.8, 46);
 
             gyroTurn.absolute(-90);
-            drive.forward(0.5,20 - (int)robot.rearDistanceSensor.getDistance(DistanceUnit.INCH));
+            drive.forward(0.5,24 - (int)robot.rearDistanceSensor.getDistance(DistanceUnit.INCH));
             robot.leftGrabberServo.setPosition(robot.LEFT_GRABBER_CLOSED);
             robot.rightGrabberServo.setPosition(robot.RIGHT_GRABBER_CLOSED);
             gyroTurn.absolute(165);
+            drive.forward(0.7,46);
 //            strafe.right(0.5, 48 - (int)robot.getDistanceToWall(10));
             //left = 15, rear = 20
 //            wobble.moveArm(robot.WOBBLE_ARM_DOWN);
+        }
+        else if (target == WobbleTarget.B) {
+            drive.backward(0.8, 75);
+            gyroTurn.absolute(0);
+            strafe.left(0.5, ((int)robot.getDistanceToWall(robot.leftDistanceSensor, robot.leftDistanceSensorTwo,
+                    15) - 18));
+            wobble.moveArm(robot.WOBBLE_ARM_DOWN);
+            Thread.sleep(1000);
+            wobble.release();
+            wobble.moveArm(robot.WOBBLE_ARM_UP);
+            Thread.sleep(500);
+        }
+        else { //WobbleTarget.C
+            drive.backward(0.8, 50);
+            gyroTurn.absolute(5);
+            drive.backward(0.8, 46);
+            wobble.moveArm(robot.WOBBLE_ARM_DOWN);
+            Thread.sleep(1000);
+            wobble.release();
+            wobble.moveArm(robot.WOBBLE_ARM_UP);
+            Thread.sleep(500);
+            gyroTurn.absolute(0);
+
+            robot.rightGrabberServo.setPosition(robot.RIGHT_GRABBER_OPEN);
+            robot.leftGrabberServo.setPosition(robot.LEFT_GRABBER_OPEN);
+            robot.scoopServo.setPosition(robot.SCOOPING_POSITION);
+            drive.forward(0.8, 41);
+            gyroTurn.absolute(0);
+            drive.forward(0.8, 55);
+            drive.forward(0.6,(int)robot.getDistanceToWall(robot.frontDistanceSensor,
+                    16) - 16);
+
+            gyroTurn.absolute(-90);
+            drive.forward(0.5,24 - (int)robot.rearDistanceSensor.getDistance(DistanceUnit.INCH));
+            robot.leftGrabberServo.setPosition(robot.LEFT_GRABBER_CLOSED);
+            robot.rightGrabberServo.setPosition(robot.RIGHT_GRABBER_CLOSED);
+            gyroTurn.absolute(-5);
+//            drive.backward(0.7,94);
+//            gyroTurn.absolute(100);
+//            drive.forward(0.6,6);
         }
     }
 
